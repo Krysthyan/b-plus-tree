7d457d692803cf644cb9ac5c7f05f18df1a17652
Krysthyan <krysthyan_09h@yahoo.com>
2016-08-01 01:24:49 -0500

    clean code

--- src/arbolb/ArbolB.java
+++ src/arbolb/ArbolB.java
@@ -1,55 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package arbolb;
 
 import arbolb.arbol_mas.ArbolException;
 import arbolb.arbol_mas.DepositoArchivos;
-import arbolb.arbol_mas.Repositorio;
 import arbolb.arbol_mas.SerializadorException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import sun.applet.Main;
-
-/**
- *
- * @author krysthyan
- */
 
 
 public class ArbolB {
 
     /**
      * @param args the command line arguments
+     * @throws arbolb.arbol_mas.SerializadorException
+     * @throws java.io.IOException
      */
     public static void main(String[] args) throws SerializadorException, IOException {
-        int validar=0;
+        int validar = 0;
         int opcion;
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         try {
             DepositoArchivos<String,Vehiculo> vehiculos=new DepositoArchivos("src/","vehiculos",1000);
-            while(validar==0){
+            while(validar == 0){
                 menu();
                 opcion=INT(br.readLine());
-                if(opcion==1){
+                if(opcion == 1){
                     System.out.println("Ingrese la placa de su vehiculo ");
                     String placa=br.readLine();
                     System.out.println("Ingrese la marca de su vehiculo");
                     String marca=br.readLine();
                     vehiculos.agregar(placa, new Vehiculo(placa,marca));
                 }
-                if(opcion==2){
+                if(opcion == 2){
                     System.out.println("Ingrese la placa que quiere buscar");
                     String placa=br.readLine();
                     System.out.println("resultado de funcion buscar "+vehiculos.exists(placa));
                 }
-                if(opcion==3){
+                if(opcion == 3){
                     System.out.println("Ingrese la placa de su vehiculo que quiere modificar ");
                     String placaModificar=br.readLine();
                     System.out.println("Ingrese la placa de su vehiculo nueva");
@@ -58,31 +46,31 @@ public class ArbolB {
                     String marca=br.readLine();
                     vehiculos.modificar(placaModificar, new Vehiculo(placa,marca));
                 }
-                if(opcion==4){
+                if(opcion == 4){
                     System.out.println("Ingrese la placa que quiere eliminar");
                     String placa=br.readLine();
                     vehiculos.eliminar(placa);
                 }
-                if(opcion==5){
+                if(opcion == 5){
                     System.out.println("Ingrese la placa que quiere obtener");
                     String placa=br.readLine();
                     Vehiculo leerVehiculo=vehiculos.get(placa);
                     System.out.println("Vehiculo de clace abs   "+leerVehiculo.toString());
                 }
-                if(opcion==6){
+                if(opcion == 6){
                     List<Vehiculo> lista = vehiculos.listar();
                     System.out.println("\nLista");
-                    for(Vehiculo vehiculo: lista){
+                    lista.stream().forEach((vehiculo) -> {
                         System.out.println(vehiculo.toString());
-                    }
+                    });
                 }
-                if(opcion==7){
-                    validar=1;
+                if(opcion == 7){
+                    validar = 1;
                 }
             }
 
         } catch (ArbolException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Ha ocurrido  un error: "+ex.getMessage());
         }
     }
     public static int INT(String numero){
